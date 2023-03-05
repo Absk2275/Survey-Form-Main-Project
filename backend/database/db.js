a9e18461cfd3c53531c6c3ae1498b67101ccb542
Absk2275 <106377006+Absk2275@users.noreply.github.com>
2023-03-05 17:34:28 +0530

    Update db.js

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,7 +1,7 @@
 
 const mongoose=require('mongoose')
 mongoose.set('strictQuery', false)
-const uri = `mongodb+srv://admin:admin@cluster0.as6tt.mongodb.net/?retryWrites=true&w=majority`
+const uri = `mongodb+srv://InstaClone:InstaClone@cluster0.iph4plx.mongodb.net/?retryWrites=true&w=majority`
 async function getConnection() {
     await mongoose.connect(uri, (err)=>{
         if(err){
