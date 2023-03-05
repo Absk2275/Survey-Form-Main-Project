96c66a16ad6a2962eb8c8ab7f3e61087e95cf18e
Absk2275 <abskgupta11@gmail.com>
2023-03-05 16:47:16 +0530

    Suervey-Form

---
+++ backend/routes/createSurvey.js
@@ -0,0 +1,52 @@
+
+const express = require("express");
+const surveyModel = require("../models/surveySchema");
+const router = express.Router();
+var bodyParser = require('body-parser')
+const verify = require("../auth/auth");
+router.use(bodyParser.urlencoded({ extended: false }))
+router.use(bodyParser.json())
+
+router.post("/createsurvey", verify,async(req, res)=> {
+    console.log('create survey fetched')
+    try {
+        if(req.body){
+            console.log(req.body)
+            const {name, startDate, endDate, description, otherCriteria, type,form_id}= req.body;
+            req.user;
+            const surveyresult = await surveyModel.create ({
+                name,
+                startDate,
+                endDate,
+                description,
+                otherCriteria,
+                type,
+                form_id,
+                madeBy:req.user
+            })
+
+            if(surveyresult){
+                console.log("yes nothing in here")
+            }
+            return res.status(201).json({
+                status: "Success",
+                message: surveyresult
+            })
+        }
+        else{
+            return res.send("Enter the correct details")
+        }
+
+    }
+    catch(error) {
+        console.log(error);
+        return res.status(500).json({
+            status: "Failed",
+            message: error.message
+        });
+    }
+    
+})
+
+module.exports = router;
+
